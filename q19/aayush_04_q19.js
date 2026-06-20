2727cf1e097351c5d83bc7a2981b23c744fcc8cd
aayushlabs <infoataayush125@gmail.com>
2026-06-20 16:10:53 +0545

    Q19 screen information

---
+++ q19/aayush_04_q19.js
@@ -0,0 +1,7 @@
+let info = "";
+
+info += "Screen Width: " + screen.width + "px<br>";
+info += "Screen Height: " + screen.height + "px<br>";
+info += "Available Screen Width: " + screen.availWidth + "px";
+
+document.getElementById("output").innerHTML = info;
